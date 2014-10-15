51f1ba99cc3bd04f06db34c145952494779a6e48
Michael Wallner <mike@php.net>
2014-10-15 10:33:55 +0200

    update from docs

--- lib/pq/Query/WriterInterface.php
+++ lib/pq/Query/WriterInterface.php
@@ -22,17 +22,20 @@ interface WriterInterface
 	
 	/**
 	 * Returns a list any types associated with the params
+	 * @return array
 	 */
 	function getTypes();
 	
 	/**
 	 * Reset the state of the query writer
+	 * @return \pq\Query\WriterInterface
 	 */
 	function reset();
 	
 	/**
 	 * Write plain SQL to the query
-	 * @param mixed $arg variable list of arguments, arrays will be imploded to a comm separated list
+	 * @param mixed $arg variable list of arguments, arrays will be imploded to a comma separated list
+	 * @return \pq\Query\WriterInterface
 	 */
 	function write(/*...*/);
 	
@@ -40,6 +43,7 @@ interface WriterInterface
 	 * Remember the parameter with any associated type and return $N to be written to the query string
 	 * @param mixed $param a literal parameter, a \pq\Gateway\Table\Cell or a \pq\Query\Expr
 	 * @param int $type the oid of the type of the param
+	 * @return \pq\Query\WriterInterface
 	 */
 	function param($param, $type = null);
 	
